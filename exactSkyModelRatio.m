54d14f0db1ce4b5f51759b486816ca1a74c019c2
Ondřej Krsička <ondrej.krsicka@gmail.com>
2023-06-20 15:46:28 +0200

    Squashed 'webcamCalibration/skyModel/' content from commit acd98f9
    git-subtree-dir: webcamCalibration/skyModel
    git-subtree-split: acd98f932b6d83e2b4199f0e24cf11352f465201

---
+++ exactSkyModelRatio.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  function ratio = exactSkyModelRatio(a, b, c, d, e, f, up, vp, vh, lz, phi, phiSun, thetaSun)
+%    Synthesizes the full sky model, based on ratios of luminances
+% 
+% Input parameters:
+%  - a, b, c, d, e: Perez sky model parameters
+%  - f: camera focal length (in pixels)
+%  - up: x-coordinates of pixels in image
+%  - vp: y-coordinates of pixels in image
+%  - theta: camera zenith angle
+%  - lz: zenith luminance
+%  - phi: camera azimuth angle (in radians)
+%  - phiSun: sun azimuth angle (in radians)
+%  - thetaSun: sun zenith angle (in radians)
+%
+% Output parameters:
+%  - ratio: ratio of luminances (with respect to zenith)
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ratio = exactSkyModelRatio(a, b, c, d, e, f, up, vp, lz, theta, phi, phiSun, thetaSun)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2006-2009 Jean-Francois Lalonde
+% Carnegie Mellon University
+% Do not distribute
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ratio = lz .* exactSkyModel(a, b, c, d, e, f, up, vp, theta, phi, phiSun, thetaSun) ./ perezSkyModel(a, b, c, d, e, 0, thetaSun);

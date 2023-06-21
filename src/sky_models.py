c4096b7fb89f1133327363aca907f19f1add2bb4
Ondřej Krsička <ondrej.krsicka@gmail.com>
2023-06-21 13:48:03 +0200

    added default theta_s, phi_s, phi_c according to Lalonde Appendix C

--- src/sky_models.py
+++ src/sky_models.py
@@ -7,11 +7,21 @@ from coordinates import CoordinateConvertor
 class SkyModel:
     def __init__(self, W, H) -> None:
         self.convertor = CoordinateConvertor(W, H)
+        self.default_camera_azimuth = 0
+        self.default_sun_azimuth = np.deg2rad(120)
+        self.default_sun_zenith = np.deg2rad(80)
 
     def model_raw(self, theta, gamma):
         ...
 
     def model(self, camera_azimuth, camera_zenith, f, sun_azimuth, sun_zenith, x, y):
+        if sun_azimuth is None:
+            sun_azimuth = self.default_sun_azimuth
+        if sun_zenith is None:
+            sun_zenith = self.default_sun_zenith
+        if camera_azimuth is None:
+            camera_azimuth = self.default_camera_azimuth
+            
         theta, gamma = self.convertor.xy_to_theta_gamma(
             x, y, camera_azimuth, camera_zenith, f, sun_azimuth, sun_zenith)
         return self.model_raw(theta, gamma)
@@ -40,6 +50,8 @@ class PragueSkyModel(SkyModel):
         return batch_luminance(self.sun_elevation, self.visibility, self.albedo, theta, gamma)
 
     def model(self, camera_azimuth, camera_zenith, f, sun_azimuth, sun_zenith, x, y):
+        if sun_zenith is None:
+            sun_zenith = self.default_sun_zenith
         self.sun_elevation = np.pi/2 - sun_zenith
         value = super().model(camera_azimuth, camera_zenith, f, sun_azimuth, sun_zenith, x, y)
         return np.array(value)
@@ -53,12 +65,26 @@ class PerezSkyModel(SkyModel):
         a, b, c, d, e = -1, -0.32, 10, -3, 0.45
         l_p = (1 + a * np.exp(b / np.cos(theta))) * \
             (1 + c * np.exp(d * gamma) + e * np.cos(gamma)**2)
-        return np.nan_to_num(l_p)
+        return np.nan_to_num(l_p, nan=0, posinf=1000, neginf=-1000)
 
 
 class PerezAzimuthIndependentSkyModel(SkyModel):
     def __init__(self, W, H) -> None:
-        self.convertor = CoordinateConvertor(W, H)
+        super().__init__(W, H)
 
-    def model_raw(self, theta, gamma):
-        return np.nan_to_num(1 - np.exp(-0.32/np.cos(theta)))
+    def model_raw(self, theta):
+        return np.nan_to_num(1 - np.exp(-0.32/np.cos(theta)), nan=0, posinf=1000, neginf=-1000)
+    
+    
+    def model(self, camera_azimuth, camera_zenith, f, sun_azimuth, sun_zenith, x, y):
+        u, v = self.convertor.xy_to_uv(x, y)
+        theta = self.convertor.point_zenith(u, v, camera_zenith, f)
+        return self.model_raw(theta)
+    
+    
+    def generate_image(self, camera_zenith, f):
+        x, y = np.ones([self.convertor.W, self.convertor.H]).nonzero()
+        r = self.model(None, camera_zenith, f, None, None, x, y)
+        img = np.zeros([self.convertor.H, self.convertor.W])
+        img[y, x] = r
+        return img

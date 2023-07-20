1f89d06879486593134ff767bc4077854fe206a5
Ondřej Krsička <ondrej.krsicka@gmail.com>
2023-07-20 10:29:24 +0200

    line endings

--- src/chmucalib.sh
+++ src/chmucalib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # arg $1 is location and arg $2 is model (perez or prague)
 timeout 1h python3 main.py -mI perez-azimuth-independent -mJ "$2" -n 1000 -W 720 -H 540 -f0 500 -df date-time -I ../data/I/"$1" -J ../data/J/"$1" -m ../data/masks/"$1".jpg -min 10 -max 240 -webcams ../data/webcams.json || echo "$1 TIMED OUT"
+ 
